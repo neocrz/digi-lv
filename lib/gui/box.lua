770bbcb351a056338d4c9ad39b8671bbb0ac46a4
neocrz <neo59crz@gmail.com>
2024-10-23 17:23:13 -0300

    .

--- lib/gui/box.lua
+++ lib/gui/box.lua
@@ -103,7 +103,7 @@ end
 
 function _.List:touchmoved(id, x, y, dx, dy, pressure)
   if col.Rect({ x = x, y = y }, { x = self.x, y = self.y, w = self.w, h = self.h }) then
-    self.moveRef(dy)
+    self:moveRef(dy)
   end
   for k, obj in pairs(self.objsD) do
     if obj.touchmoved then obj:touchmoved(id, x, y, dx, dy, pressure) end
